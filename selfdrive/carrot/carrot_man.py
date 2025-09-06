3fe0c3e6f14194fde20eee0e26b39d6870ae79ee
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-09-06 20:09:58 +0800

    导航调试信息格式化为1个小数点

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1719,9 +1719,9 @@ class CarrotServ:
       atc_desired = min(atc_desired, self.calculate_current_speed(x_dist_to_turn - atc_dist, atc_speed, safe_sec, decel))
 
     if (self.showDebugLog & 1) > 0:
-      debugText = (f"***atc info: type={atc_type_org},{atc_type},xdist={x_dist_to_turn},dist={atc_dist:.1f}," +
-                   f"start={atc_start_dist:.1f},speed={atc_speed_org:.1f},{atc_speed:.1f},decal_dist={do_speed_decal_dist}" +
-                   f"fork_dist_offset={fork_dist_offset},do_fork_dist={do_fork_dist},do_fork_nav_dist={do_fork_nav_dist}," +
+      debugText = (f"***atc info: type={atc_type_org},{atc_type},xdist={x_dist_to_turn:.1f},dist={atc_dist:.1f}," +
+                   f"start={atc_start_dist:.1f},speed={atc_speed_org:.1f},{atc_speed:.1f},decal_dist={do_speed_decal_dist:.1f}" +
+                   f"fork_dist_offset={fork_dist_offset:.1f},do_fork_dist={do_fork_dist:.1f},do_fork_nav_dist={do_fork_nav_dist:.1f}," +
                    f"atc_paused={self.atc_paused}"
                    )
       print(debugText)

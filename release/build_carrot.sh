01410a22d3c8400ac474357920d5b708a812a3fc
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-09-14 23:30:36 +0800

    修改打包脚本

--- release/build_carrot.sh
+++ release/build_carrot.sh
@@ -2,11 +2,13 @@
 
 BUILD_DIR=/data/openpilot
 cd $BUILD_DIR
+rm -rf .git
 git init
-#git remote add origin https://github.com/ajouatom/openpilot
-git remote set-url --push origin https://github.com/ajouatom/openpilot.git
+git remote add origin https://jihulab.com/fishop/openpilot.git
+#git remote set-url --push origin https://jihulab.com/fishop/openpilot.git
 
 # Cleanup
+echo "cleanup"
 find . -name '*.a' -delete
 find . -name '*.o' -delete
 find . -name '*.os' -delete
@@ -14,13 +16,14 @@ find . -name '*.pyc' -delete
 find . -name 'moc_*' -delete
 find . -name '__pycache__' -delete
 rm -rf .sconsign.dblite Jenkinsfile release/
-rm selfdrive/modeld/models/*.onnx
+rm -rf selfdrive/modeld/models/*.onnx
 touch prebuilt
 
 # Add built files to git
+echo "add all file"
 git add -f .
 
 VERSION="carrot_v$(date +%y%m%d)"
 git commit -m $VERSION
-git branch -m $VERSION
-git push -f origin $VERSION
+git branch -m "escc-cp"
+git push -f origin "escc-cp"
